afab589c41c945a2e55f9f6c7e7f57f4f0067fd6
sxsw1029 <sxsw1029@qq.com>
2025-06-06 23:00:00 +0800

    Lab01 Exercise 5

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,20 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
+    if (head == NULL || head->next == NULL) {
+        return 0;
+    }
+
+    node *tortoise = head;
+    node *hare = head;
+
+    while (hare != NULL && hare->next != NULL) {
+        hare = hare->next->next;
+
+        tortoise = tortoise->next;
+
+        if (tortoise == hare) return 1;
+    }
+
     return 0;
 }
