90f0cc40a4d243b4f50ac3860fbbefbe7a1d2fd3
sxsw1029 <sxsw1029@qq.com>
2025-06-07 01:34:29 +0800

    refactor: optimize code

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -8,7 +8,7 @@
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
 
-    uint16_t n = (*reg & 0x1) ^ (*reg >> 2 & 0x1) ^ (*reg >> 3 & 0x1) ^ (*reg >> 5 & 0x1);
+    uint16_t n = ((*reg >> 0) ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 0x1;
 
     *reg = (*reg >> 1) | (n << 15);
 }
