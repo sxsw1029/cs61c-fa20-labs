53ec906469167e2fdea5194bbd8f5976437e4b0f
sxsw1029 <sxsw1029@qq.com>
2025-06-07 01:28:28 +0800

    Lab02 Exercise 2

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,11 @@
 #include <string.h>
 #include "lfsr.h"
 
+
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
-}
 
+    uint16_t n = (*reg & 0x1) ^ (*reg >> 2 & 0x1) ^ (*reg >> 3 & 0x1) ^ (*reg >> 5 & 0x1);
+
+    *reg = (*reg >> 1) | (n << 15);
+}
